9730e9c40c05656da6eadc8c5751afd6f6c1126b
Sigfried Hache <sigfriedhache@gmail.com>
2024-05-21 14:52:34 -0400

    added a formatting function updated gitignore

--- astro_utils/formatting.py
+++ astro_utils/formatting.py
@@ -1,4 +1,46 @@
+import astropy.units as u
+
+from astropy.coordinates import SkyCoord
+from gala.coordinates import GD1Koposov10, reflex_correct
+
 
 def skycoord_to_string(skycoord):
     return ' '.join(skycoord.to_string()).replace(' ',', ')
 
+
+def make_dataframe(table):
+    """Transform coordinates from ICRS to GD-1 frame.
+    
+    table: Astropy Table
+    
+    returns: Pandas DataFrame
+    """
+    #Create a SkyCoord object with the coordinates and proper motions
+    # in the input table
+    skycoord = SkyCoord(
+               ra=table['ra'], 
+               dec=table['dec'],
+               pm_ra_cosdec=table['pmra'],
+               pm_dec=table['pmdec'], 
+               distance=8*u.kpc, 
+               radial_velocity=0*u.km/u.s)
+
+    # Define the GD-1 reference frame
+    gd1_frame = GD1Koposov10()
+
+    # Transform input coordinates to the GD-1 reference frame
+    transformed = skycoord.transform_to(gd1_frame)
+
+    # Correct GD-1 coordinates for solar system motion around galactic center
+    skycoord_gd1 = reflex_correct(transformed)
+
+    #Add GD-1 reference frame columns for coordinates and proper motions
+    table['phi1'] = skycoord_gd1.phi1
+    table['phi2'] = skycoord_gd1.phi2
+    table['pm_phi1'] = skycoord_gd1.pm_phi1_cosphi2
+    table['pm_phi2'] = skycoord_gd1.pm_phi2
+    
+    # Create DataFrame
+    df = table.to_pandas()
+
+    return df
